9c51dbfe36bdb46b91d5032f2c12c7b98a8df615
yaleman <yaleman@ricetek.net>
2020-05-17 13:09:53 +1000

    fixed pylint complaining in metadata

--- dewar/metadata/__init__.py
+++ dewar/metadata/__init__.py
@@ -1,15 +1,10 @@
 """ base metadata store class, uses tinyDB """
 
-import logging
-logger = logging.getLogger()
+from tinydb import TinyDB, Query
+from loguru import logger
 
 from ..constants import METADATA_TYPES
 
-try:
-    from tinydb import TinyDB, Query
-    from loguru import logger
-except ImportError as import_error:
-    logger.error(f"Unable to import a required package: {import_error}")
 
 
 class MetadataStore():
@@ -21,21 +16,20 @@ class MetadataStore():
             logger.error(error_message)
             raise ValueError(error_message)
         
-        self.db = TinyDB(self.filename)
+        self.tinydb = TinyDB(self.filename)
 
         # pre-create all the default tables
         self.dbtable = {}
         for table in METADATA_TYPES:
-            self.dbtable[table] = self.db.table(name=table)
+            self.dbtable[table] = self.tinydb.table(name=table)
 
     def get_hash(self, filehash: str):
         """ base implementation """
-        FileHash = Query()
-        result = self.dbtable['file'].search(FileHash.filehash==filehash)
-        if len(result) > 1:
-            raise ValueError(f"More than one result returned for filehash, that's bad? {result}")
-        elif not result:
+        result = self.dbtable['file'].search(Query().filehash==filehash)
+        if not result: #pylint: disable=no-else-return
             return False
+        elif len(result) > 1:
+            raise ValueError(f"More than one result returned for filehash, that's bad? {result}")
         else:
             return result[0]
         
@@ -57,7 +51,7 @@ class MetadataStore():
         """ generic metadata inserter - only required thing is type to delineate from other fields - as it is the table name """
         if metadata_type not in self.dbtable:
             logger.debug(f"Metadata insert for a table that hasn't been used before: {metadata_type}")
-            table = self.db.table(name=metadata_type)
+            table = self.tinydb.table(name=metadata_type)
         else:
             table = self.dbtable.get(metadata_type)
         return table.insert(kwargs)
@@ -70,18 +64,19 @@ class MetadataStore():
             """
         if metadata_type not in self.dbtable:
             logger.debug(f"Metadata insert for a table that hasn't been used before: {metadata_type}")
-            table = self.db.table(name=metadata_type)
+            table = self.tinydb.table(name=metadata_type)
         else:
             table = self.dbtable.get(metadata_type)
         
-        if not kwargs:
-            logger.error("No search term provided, returning False")
-            return False
-        elif len(kwargs) > 1:
+        if len(kwargs) > 1:
             logger.warning("More than one search term provided, this only supports the first provided one for now!")
         
-        first_kwarg = list(kwargs.keys())[0]
-        result = table.search(Query()[first_kwarg] == kwargs[first_kwarg])
-        return result
+        if not kwargs: #pylint: disable=no-else-return
+            logger.error("No search term provided, returning False")
+            retval = False
+        else:
+            first_kwarg = list(kwargs.keys())[0]
+            retval = table.search(Query()[first_kwarg] == kwargs[first_kwarg])
+        return retval
     
     # TODO: dewar.metadata.put_hash del_hash ?
